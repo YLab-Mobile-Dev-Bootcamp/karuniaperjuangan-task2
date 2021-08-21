78a24b85cef6b2c8d0b09bbacf4aef0e8e3a855d
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2021-08-21 16:17:33 +0700

    revisi task4

--- lib/ongkir_api.dart
+++ lib/ongkir_api.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
@@ -10,36 +8,29 @@ import 'package:http/http.dart';
 
 Future<List<String>> getKotaList() async {
   List<String> returnList = [];
-  final response = await Dio().get(
-    "https://api.rajaongkir.com/starter/city",
-    options: Options(
-      headers: {
-      "key": "237d4652a598ea4bd9ac7047bba33a90"
-        },
-      responseType: ResponseType.plain
-      )
-    );
-  if(response.statusCode ==200){
+  final response = await Dio().get("https://api.rajaongkir.com/starter/city",
+      options: Options(
+          headers: {"key": "237d4652a598ea4bd9ac7047bba33a90"},
+          responseType: ResponseType.plain));
+  if (response.statusCode == 200) {
     final obj = jsonDecode(response.data);
     final rajaobj = obj["rajaongkir"];
     List<dynamic> cityList = rajaobj["results"];
-    cityList.forEach((city){ returnList.add(city["city_name"].toString());});
+    cityList.forEach((city) {
+      returnList.add(city["city_name"].toString());
+    });
   }
   return returnList;
 }
 
 Future<List<dynamic>> getKotaIdList() async {
   List<String> returnList = [];
-  final response = await Dio().get(
-    "https://api.rajaongkir.com/starter/city",
-    options: Options(
-      headers: {
-      "key": "237d4652a598ea4bd9ac7047bba33a90"
-        },
-      responseType: ResponseType.plain
-      )
-    );;
-  if(response.statusCode ==200){
+  final response = await Dio().get("https://api.rajaongkir.com/starter/city",
+      options: Options(
+          headers: {"key": "237d4652a598ea4bd9ac7047bba33a90"},
+          responseType: ResponseType.plain));
+  ;
+  if (response.statusCode == 200) {
     final obj = jsonDecode(response.data);
     final rajaobj = obj["rajaongkir"];
     List<dynamic> cityList = rajaobj["results"];
@@ -48,33 +39,33 @@ Future<List<dynamic>> getKotaIdList() async {
   return returnList;
 }
 
-Future<List<String>> GetLayananList({required int idAsal,required int idTujuan,required  int berat,required  String kurir}) async
-{
+Future<List<String>> GetLayananList(
+    {required int idAsal,
+    required int idTujuan,
+    required int berat,
+    required String kurir}) async {
   List<String> returnList = [];
-  final response = await Dio().post(
-    "https://api.rajaongkir.com/starter/cost",
-    data: {
-      'origin': '$idAsal',
-      'destination' : '$idTujuan',
-      'weight': berat,
-      'courier': kurir
-    },
-    options: Options(
-      headers: {
-        "key": "237d4652a598ea4bd9ac7047bba33a90",
-        "content-type": "application/x-www-form-urlencoded"
+  final response = await Dio().post("https://api.rajaongkir.com/starter/cost",
+      data: {
+        'origin': '$idAsal',
+        'destination': '$idTujuan',
+        'weight': berat,
+        'courier': kurir
       },
-      contentType: Headers.formUrlEncodedContentType,
-      responseType: ResponseType.plain
-    )
-    );
-  if(response.statusCode !=200) return returnList;
+      options: Options(
+          headers: {
+            "key": "237d4652a598ea4bd9ac7047bba33a90",
+            "content-type": "application/x-www-form-urlencoded"
+          },
+          contentType: Headers.formUrlEncodedContentType,
+          responseType: ResponseType.plain));
+  if (response.statusCode != 200) return returnList;
   final obj = jsonDecode(response.data);
   final rajaObj = obj["rajaongkir"];
   List<dynamic> resultList = rajaObj["results"];
   final courierInfo = resultList[0];
   List<dynamic> servicesList = courierInfo["costs"];
-  servicesList.forEach((service){
+  servicesList.forEach((service) {
     String layanan = courierInfo["name"].toString();
     layanan += "-" + service["service"].toString();
     List<dynamic> detail = service["cost"];
@@ -82,8 +73,7 @@ Future<List<String>> GetLayananList({required int idAsal,required int idTujuan,r
     layanan += " Rp" + (infoService["value"]).toString();
     layanan += " " + infoService["etd"] + " Hari";
     returnList.add(layanan);
-   }
-   );
+  });
 
   return returnList;
 }
--- lib/ongkir_page.dart
+++ lib/ongkir_page.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:ylabs_task2/ongkir_api.dart';
 
-class OngkirPage extends StatefulWidget{
-
+class OngkirPage extends StatefulWidget {
   @override
   _OngkirPageState createState() => _OngkirPageState();
 }
 
 class _OngkirPageState extends State<OngkirPage> {
-
   List<String> listKota = [];
   List<dynamic> listKotaId = [];
   List<String> daftarLayanan = [];
@@ -18,57 +16,80 @@ class _OngkirPageState extends State<OngkirPage> {
   String _kurirValue = "jne";
   String keteranganLayanan = "";
 
-
-
-
   @override
   Widget build(BuildContext context) {
     getData();
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.all(40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //Text(listKota.toString()),
-            Autocomplete<String>(
-              optionsBuilder: (TextEditingValue value){
-                return listKota.where((element) => element.toLowerCase().startsWith(value.text.toLowerCase())).toList();
-              },
-              fieldViewBuilder: (context,textController,focusNode,onfieldSubmitted) => TextField(
-                controller: textController,
-                focusNode: focusNode,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Asal Kota"
-                ),
-                onChanged: (String value){ setState(() {_asalKotaController.value = TextEditingValue(text: value);}); },
-              ),
-              onSelected: (String value){ setState(() {_asalKotaController.value = TextEditingValue(text: value);}); },
-              ),
-              SizedBox(height: 24,),
+          padding: EdgeInsets.all(40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //Text(listKota.toString()),
               Autocomplete<String>(
-              optionsBuilder: (TextEditingValue value){
-                return listKota.where((element) => element.toLowerCase().startsWith(value.text.toLowerCase())).toList();
-              },
-              fieldViewBuilder: (context,textController,focusNode,onfieldSubmitted) => TextField(
-                controller: textController,
-                focusNode: focusNode,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Kota Tujuan"
+                optionsBuilder: (TextEditingValue value) {
+                  return listKota
+                      .where((element) => element
+                          .toLowerCase()
+                          .startsWith(value.text.toLowerCase()))
+                      .toList();
+                },
+                fieldViewBuilder:
+                    (context, textController, focusNode, onfieldSubmitted) =>
+                        TextField(
+                  controller: textController,
+                  focusNode: focusNode,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Asal Kota"),
+                  onChanged: (String value) {
+                    setState(() {
+                      _asalKotaController.value = TextEditingValue(text: value);
+                    });
+                  },
                 ),
-                onChanged: (String value){ setState(() {_tujuanController.value = TextEditingValue(text: value);}); },
+                onSelected: (String value) {
+                  setState(() {
+                    _asalKotaController.value = TextEditingValue(text: value);
+                  });
+                },
               ),
-              onSelected: (String value){ setState(() {_tujuanController.value = TextEditingValue(text: value);}); },
+              SizedBox(
+                height: 24,
               ),
-              SizedBox(height: 24,),
+              Autocomplete<String>(
+                optionsBuilder: (TextEditingValue value) {
+                  return listKota
+                      .where((element) => element
+                          .toLowerCase()
+                          .startsWith(value.text.toLowerCase()))
+                      .toList();
+                },
+                fieldViewBuilder:
+                    (context, textController, focusNode, onfieldSubmitted) =>
+                        TextField(
+                  controller: textController,
+                  focusNode: focusNode,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Kota Tujuan"),
+                  onChanged: (String value) {
+                    setState(() {
+                      _tujuanController.value = TextEditingValue(text: value);
+                    });
+                  },
+                ),
+                onSelected: (String value) {
+                  setState(() {
+                    _tujuanController.value = TextEditingValue(text: value);
+                  });
+                },
+              ),
+              SizedBox(
+                height: 24,
+              ),
               TextField(
                 controller: _beratController,
                 decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Berat (gram)"
-                ),
+                    border: OutlineInputBorder(), labelText: "Berat (gram)"),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -76,63 +97,94 @@ class _OngkirPageState extends State<OngkirPage> {
                   Row(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Radio(value: 'jne', groupValue: _kurirValue, onChanged: (String? value){setState(() {_kurirValue = value??"";});}),
+                      Radio(
+                          value: 'jne',
+                          groupValue: _kurirValue,
+                          onChanged: (String? value) {
+                            setState(() {
+                              _kurirValue = value ?? "";
+                            });
+                          }),
                       Text("JNE")
-                    ],),
+                    ],
+                  ),
                   Row(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Radio(value: 'pos', groupValue: _kurirValue, onChanged: (String? value){setState(() {_kurirValue = value??"";});}),
+                      Radio(
+                          value: 'pos',
+                          groupValue: _kurirValue,
+                          onChanged: (String? value) {
+                            setState(() {
+                              _kurirValue = value ?? "";
+                            });
+                          }),
                       Text("POS Kilat")
-                    ],),
+                    ],
+                  ),
                   Row(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Radio(value: 'tiki', groupValue: _kurirValue, onChanged: (String? value){setState(() {_kurirValue = value??"";});}),
+                      Radio(
+                          value: 'tiki',
+                          groupValue: _kurirValue,
+                          onChanged: (String? value) {
+                            setState(() {
+                              _kurirValue = value ?? "";
+                            });
+                          }),
                       Text("Tiki")
-                    ],), 
+                    ],
+                  ),
                 ],
               ),
-              SizedBox(height: 24,),
+              SizedBox(
+                height: 24,
+              ),
               ElevatedButton(
-                onPressed: () async{
-                  if(_beratController.text.isEmpty || _asalKotaController.text.isEmpty || _tujuanController.text.isEmpty) return;
-                  daftarLayanan = await GetLayananList(
-                    idAsal: getIdKota(_asalKotaController.text),
-                    idTujuan: getIdKota(_tujuanController.text),
-                    berat: int.parse(_beratController.text),
-                    kurir: _kurirValue);
-                  keteranganLayanan = "";
-                  daftarLayanan.forEach((layanan){
-                    keteranganLayanan += "-" + layanan +"\n";
-                  });
-                  showDialog(context: context, builder: (context) => AlertDialog(
-                    title: Text("Ongkos Kirim"),
-                    content: Text(keteranganLayanan),
-                  ));
+                  onPressed: () async {
+                    if (_beratController.text.isEmpty ||
+                        _asalKotaController.text.isEmpty ||
+                        _tujuanController.text.isEmpty) return;
+                    daftarLayanan = await GetLayananList(
+                        idAsal: getIdKota(_asalKotaController.text),
+                        idTujuan: getIdKota(_tujuanController.text),
+                        berat: int.parse(_beratController.text),
+                        kurir: _kurirValue);
+                    keteranganLayanan = "";
+                    daftarLayanan.forEach((layanan) {
+                      keteranganLayanan += "-" + layanan + "\n";
+                    });
+                    showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                              title: Text("Ongkos Kirim"),
+                              content: Text(keteranganLayanan),
+                            ));
                   },
-                child: Container(padding: EdgeInsets.all(20), child: Text("Cek Ongkir"),))
-          ],
-        )
-      ),
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    child: Text("Cek Ongkir"),
+                  ))
+            ],
+          )),
     );
   }
 
-int getIdKota(String kota){
-  int idKota = -1;
-  listKotaId.forEach((city) {
-    if((city["city_name"]).toString() == kota){
-      idKota = int.parse(city["city_id"]);
-    }
-  });
-  return idKota;
-}
+  int getIdKota(String kota) {
+    int idKota = -1;
+    listKotaId.forEach((city) {
+      if ((city["city_name"]).toString() == kota) {
+        idKota = int.parse(city["city_id"]);
+      }
+    });
+    return idKota;
+  }
 
-void getData() async{
-  setState(() async {
-    listKota = await getKotaList();
-  listKotaId = await getKotaIdList();
-  });
-}
-
+  void getData() async {
+    setState(() async {
+      listKota = await getKotaList();
+      listKotaId = await getKotaIdList();
+    });
+  }
 }
